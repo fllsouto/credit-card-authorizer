fcb23ac31e6af195aa9676d6ea136557495b94aa
Fellipe Souto <fllsouto@gmail.com>
2024-07-29 11:48:15 -0300

    <INFRA> adding spring initializr structure

---
+++ src/main/java/br/com/fllsouto/interviews/caju/creditcard/Authorizer/CreditCardAuthorizerApplication.java
@@ -0,0 +1,13 @@
+package br.com.fllsouto.interviews.caju.creditcard.Authorizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardAuthorizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardAuthorizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fllsouto/interviews/caju/creditcard/Authorizer/CreditCardAuthorizerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fllsouto.interviews.caju.creditcard.Authorizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardAuthorizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
